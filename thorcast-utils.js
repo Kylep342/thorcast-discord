c0991b2f63499aff91b74ed7c7af7cdce516c976
Kyle Pekosh <kylep342@gmail.com>
2019-03-22 23:47:23 -0500

    Supports calling for a 'random' forecast. (#1)

--- thorcast-utils.js
+++ thorcast-utils.js
@@ -2,13 +2,20 @@ const http = require('http')
 
 
 exports.processCommand = (receivedMessage, REPrefix) => {
-    const commandRegex = new RegExp(`${REPrefix} (?:(help)|(?:([a-zA-Z ]+), ?([a-zA-Z ]+),? ?([a-zA-Z ]+)?))$`, 'gm')
+    const commandRegex = new RegExp(`${REPrefix} (?:(help|random)|(?:([a-zA-Z ]+), ?([a-zA-Z ]+),? ?([a-zA-Z ]+)?))$`, 'gm')
     let matches = commandRegex.exec(receivedMessage.content)
 
     if (matches) {
         console.log('Command received')
         if (matches[1]) {
-            receivedMessage.channel.send(helpMessage())
+            switch(matches[1]) {
+                case 'help':
+                    receivedMessage.channel.send(helpMessage())
+                    break;
+                case 'random':
+                    randomForecast(receivedMessage)
+                    break;
+            }
         } else {
             forecastControl(matches, receivedMessage)
         }
@@ -37,6 +44,12 @@ function forecastControl(regexMatch, receivedMessage) {
 }
 
 
+function randomForecast(receivedMessage) {
+    url = `${THORCAST_API_URL}/api/random`
+    getForecast(url, receivedMessage)
+}
+
+
 function getForecast(url, receivedMessage) {
     http.get(url, (resp) => {
         let data = ''
@@ -56,11 +69,20 @@ function getForecast(url, receivedMessage) {
 function helpMessage() {
     return `
     Usage:
-    !thorcast|!thor|@Thorcast city, state{{, period }}
+
+    --------
+
+    Get a forecast for a chosen city, state, and period
+    (!thorcast|!thor|@Thorcast) city, state{{, period }}
 
     Examples:
     !thor Chicago, IL, Tomorrow night
     !thorcast Los Angeles, California
     @Thorcast New York City, New York, Wednesday
+
+    --------
+
+    Get a random forecast:
+    (!thorcast|!thor|@Thorcast) random
     `.replace(/^[\t ]+/gm, '').replace(/[\t ]+$/gm, '\n')
 }
