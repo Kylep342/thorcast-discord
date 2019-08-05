0523db2f688a1b1009eba33c1f891a93136b997b
Kylep342 <kylep342@gmail.com>
2019-08-05 18:03:49 -0500

    Added support for hourly forecast interaction

--- thorcast-discord.js
+++ thorcast-discord.js
@@ -1,24 +1,21 @@
-const Discord = require('discord.js')
-const Utils = require('./thorcast-utils.js')
+const Discord = require("discord.js");
+const Utils = require("./thorcast-utils.js");
 
+BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
+THORCAST_API_URL = process.env.THORCAST_API_URL;
 
-BOT_TOKEN = process.env.DISCORD_BOT_TOKEN
-THORCAST_API_URL = process.env.THORCAST_API_URL
+const client = new Discord.Client({ autoReconnect: true });
 
-const client = new Discord.Client({autoReconnect: true})
+client.on("message", receivedMessage => {
+  if (receivedMessage.author === client.user) {
+    return;
+  }
+  const REPrefix = `^(?:(?:!thor(?:cast)?)|(?:${client.user.toString()}))`;
+  const isToThorcast = new RegExp(REPrefix, "gm");
+  // handle all commands starting with !thor, !thorcast, or @Thorcast
+  if (isToThorcast.exec(receivedMessage.content)) {
+    Utils.processCommand(receivedMessage);
+  }
+});
 
-
-client.on('message', (receivedMessage) => {
-    if (receivedMessage.author === client.user) {
-        return
-    }
-    const REPrefix = `^(?:(?:!thor(?:cast)?)|(?:${client.user.toString()}))`
-    const isToThorcast = new RegExp(REPrefix, 'gm')
-    // handle all commands starting with !thor, !thorcast, or @Thorcast
-    if (isToThorcast.exec(receivedMessage.content)) {
-        Utils.processCommand(receivedMessage, REPrefix)
-    }
-})
-
-
-client.login(BOT_TOKEN)
+client.login(BOT_TOKEN);
--- thorcast-utils.js
+++ thorcast-utils.js
@@ -1,14 +1,22 @@
 const http = require("http");
 
-exports.processCommand = (message, REPrefix) => {
+// TODO: Determine architecture to support many commands
+// Do research on other bots and how they are setup
+
+exports.processCommand = message => {
   const commandRegex = new RegExp(
-    `${REPrefix} (?:(help|random)|(?:([a-zA-Z ]+), ?([a-zA-Z ]+),? ?([a-zA-Z ]+)?))$`,
+    `(?:(help|random)|(?:([a-zA-Z ]+), ?([a-zA-Z ]+),? ?([a-zA-Z ]+)?,? ?([0-9]+)?))$`,
     "gm"
   );
-  let matches = commandRegex.exec(message.content);
+  let matches = commandRegex.exec(
+    message.content
+      .split(" ")
+      .slice(1)
+      .join(" ")
+  );
 
   if (matches) {
-    console.log("Command received");
+    console.log(`Command received: ${matches}`);
     if (!matches[1]) {
       forecastControl(matches, message);
     } else {
@@ -31,25 +39,37 @@ function forecastControl(regexMatch, message) {
   let state;
   let period = false;
   let url = "";
-  if (regexMatch[4]) {
+  if (regexMatch[4] === "hourly" && regexMatch[5]) {
+    city = regexMatch[2].replace(/ /g, "+");
+    state = regexMatch[3].replace(/ /g, "+");
+    hours = regexMatch[5];
+    url = `${THORCAST_API_URL}/api/forecast/hourly/city=${city}&state=${state}&hours=${hours}`;
+    getHourlyForecast(url, message);
+  } else if (regexMatch[4] === "hourly") {
+    city = regexMatch[2].replace(/ /g, "+");
+    state = regexMatch[3].replace(/ /g, "+");
+    url = `${THORCAST_API_URL}/api/forecast/hourly/city=${city}&state=${state}`;
+    getHourlyForecast(url, message);
+  } else if (regexMatch[4]) {
     city = regexMatch[2].replace(/ /g, "+");
     state = regexMatch[3].replace(/ /g, "+");
     period = regexMatch[4].replace(/ /g, "+");
-    url = `${THORCAST_API_URL}/api/forecast/city=${city}&state=${state}&period=${period}`;
+    url = `${THORCAST_API_URL}/api/forecast/detailed/city=${city}&state=${state}&period=${period}`;
+    getDetailedForecast(url, message);
   } else {
     city = regexMatch[2].replace(/ /g, "+");
     state = regexMatch[3].replace(/ /g, "+");
-    url = `${THORCAST_API_URL}/api/forecast/city=${city}&state=${state}`;
+    url = `${THORCAST_API_URL}/api/forecast/detailed/city=${city}&state=${state}`;
+    getDetailedForecast(url, message);
   }
-  getForecast(url, message);
 }
 
 function randomForecast(message) {
-  url = `${THORCAST_API_URL}/api/forecast/random`;
-  getForecast(url, message);
+  url = `${THORCAST_API_URL}/api/forecast/detailed/random`;
+  getDetailedForecast(url, message);
 }
 
-function getForecast(url, message) {
+function getDetailedForecast(url, message) {
   http.get(url, resp => {
     let data = "";
 
@@ -62,6 +82,32 @@ function getForecast(url, message) {
         const apiResponse = JSON.parse(data);
         if (resp.statusCode === 200) {
           forecast = `${apiResponse.period}'s forecast for ${
+            apiResponse.city
+          }, ${apiResponse.state}:\n${apiResponse.forecast}`;
+          message.channel.send(forecast);
+        } else {
+          handleError(resp.statusCode, apiResponse, message);
+        }
+      } catch (e) {
+        console.error(e.message);
+      }
+    });
+  });
+}
+
+function getHourlyForecast(url, message) {
+  http.get(url, resp => {
+    let data = "";
+
+    resp.on("data", chunk => {
+      data += chunk;
+    });
+
+    resp.on("end", () => {
+      try {
+        const apiResponse = JSON.parse(data);
+        if (resp.statusCode === 200) {
+          forecast = `${apiResponse.hours} hour forecast for ${
             apiResponse.city
           }, ${apiResponse.state}:\n${apiResponse.forecast}`;
           message.channel.send(forecast);
