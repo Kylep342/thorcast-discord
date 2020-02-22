6fdfe93812da959ed853149f29fecc9a05447367
Kylep342 <kylep342@gmail.com>
2020-02-22 08:46:27 -0600

    Changes URL patterns to match new thorcast api

--- thorcast-utils.js
+++ thorcast-utils.js
@@ -4,146 +4,146 @@ const http = require("http");
 // Do research on other bots and how they are setup
 
 exports.processCommand = message => {
-  const commandRegex = new RegExp(
-    `(?:(help|random)|(?:([a-zA-Z ]+), ?([a-zA-Z ]+),? ?([a-zA-Z ]+)?,? ?([0-9]+)?))$`,
-    "gm"
-  );
-  let matches = commandRegex.exec(
-    message.content
-      .split(" ")
-      .slice(1)
-      .join(" ")
-  );
+    const commandRegex = new RegExp(
+        `(?:(help|random)|(?:([a-zA-Z ]+), ?([a-zA-Z ]+),? ?([a-zA-Z ]+)?,? ?([0-9]+)?))$`,
+        "gm"
+    );
+    let matches = commandRegex.exec(
+        message.content
+            .split(" ")
+            .slice(1)
+            .join(" ")
+    );
 
-  if (matches) {
-    console.log(`Command received: ${matches}`);
-    if (!matches[1]) {
-      forecastControl(matches, message);
+    if (matches) {
+        console.log(`Command received: ${matches}`);
+        if (!matches[1]) {
+            forecastControl(matches, message);
+        } else {
+            switch (matches[1]) {
+                case "help":
+                    message.channel.send(helpMessage());
+                    break;
+                case "random":
+                    randomForecast(message);
+                    break;
+            }
+        }
     } else {
-      switch (matches[1]) {
-        case "help":
-          message.channel.send(helpMessage());
-          break;
-        case "random":
-          randomForecast(message);
-          break;
-      }
+        return;
     }
-  } else {
-    return;
-  }
 };
 
 function forecastControl(regexMatch, message) {
-  let city;
-  let state;
-  let period = false;
-  let url = "";
-  if (regexMatch[4] === "hourly" && regexMatch[5]) {
-    city = regexMatch[2].replace(/ /g, "+");
-    state = regexMatch[3].replace(/ /g, "+");
-    hours = regexMatch[5];
-    url = `${THORCAST_API_URL}/api/forecast/hourly/city=${city}&state=${state}&hours=${hours}`;
-    getHourlyForecast(url, message);
-  } else if (regexMatch[4] === "hourly") {
-    city = regexMatch[2].replace(/ /g, "+");
-    state = regexMatch[3].replace(/ /g, "+");
-    url = `${THORCAST_API_URL}/api/forecast/hourly/city=${city}&state=${state}`;
-    getHourlyForecast(url, message);
-  } else if (regexMatch[4]) {
-    city = regexMatch[2].replace(/ /g, "+");
-    state = regexMatch[3].replace(/ /g, "+");
-    period = regexMatch[4].replace(/ /g, "+");
-    url = `${THORCAST_API_URL}/api/forecast/detailed/city=${city}&state=${state}&period=${period}`;
-    getDetailedForecast(url, message);
-  } else {
-    city = regexMatch[2].replace(/ /g, "+");
-    state = regexMatch[3].replace(/ /g, "+");
-    url = `${THORCAST_API_URL}/api/forecast/detailed/city=${city}&state=${state}`;
-    getDetailedForecast(url, message);
-  }
+    let city;
+    let state;
+    let period = false;
+    let url = "";
+    if (regexMatch[4] === "hourly" && regexMatch[5]) {
+        city = regexMatch[2].replace(/ /g, "+");
+        state = regexMatch[3].replace(/ /g, "+");
+        hours = regexMatch[5];
+        url = `${THORCAST_API_URL}/api/forecast/hourly?city=${city}&state=${state}&hours=${hours}`;
+        getHourlyForecast(url, message);
+    } else if (regexMatch[4] === "hourly") {
+        city = regexMatch[2].replace(/ /g, "+");
+        state = regexMatch[3].replace(/ /g, "+");
+        url = `${THORCAST_API_URL}/api/forecast/hourly?city=${city}&state=${state}`;
+        getHourlyForecast(url, message);
+    } else if (regexMatch[4]) {
+        city = regexMatch[2].replace(/ /g, "+");
+        state = regexMatch[3].replace(/ /g, "+");
+        period = regexMatch[4].replace(/ /g, "+");
+        url = `${THORCAST_API_URL}/api/forecast/detailed?city=${city}&state=${state}&period=${period}`;
+        getDetailedForecast(url, message);
+    } else {
+        city = regexMatch[2].replace(/ /g, "+");
+        state = regexMatch[3].replace(/ /g, "+");
+        url = `${THORCAST_API_URL}/api/forecast/detailed?city=${city}&state=${state}`;
+        getDetailedForecast(url, message);
+    }
 }
 
 function randomForecast(message) {
-  url = `${THORCAST_API_URL}/api/forecast/detailed/random`;
-  getDetailedForecast(url, message);
+    url = `${THORCAST_API_URL}/api/forecast/detailed/random`;
+    getDetailedForecast(url, message);
 }
 
 function getDetailedForecast(url, message) {
-  http.get(url, resp => {
-    let data = "";
+    http.get(url, resp => {
+        let data = "";
 
-    resp.on("data", chunk => {
-      data += chunk;
-    });
+        resp.on("data", chunk => {
+            data += chunk;
+        });
 
-    resp.on("end", () => {
-      try {
-        const apiResponse = JSON.parse(data);
-        if (resp.statusCode === 200) {
-          forecast = `${apiResponse.period}'s forecast for ${
-            apiResponse.city
-          }, ${apiResponse.state}:\n${apiResponse.forecast}`;
-          message.channel.send(forecast);
-        } else {
-          handleError(resp.statusCode, apiResponse, message);
-        }
-      } catch (e) {
-        console.error(e.message);
-      }
+        resp.on("end", () => {
+            try {
+                const apiResponse = JSON.parse(data);
+                if (resp.statusCode === 200) {
+                    forecast = `${apiResponse.period}'s forecast for ${
+                        apiResponse.city
+                        }, ${apiResponse.state}:\n${apiResponse.forecast}`;
+                    message.channel.send(forecast);
+                } else {
+                    handleError(resp.statusCode, apiResponse, message);
+                }
+            } catch (e) {
+                console.error(e.message);
+            }
+        });
     });
-  });
 }
 
 function getHourlyForecast(url, message) {
-  http.get(url, resp => {
-    let data = "";
+    http.get(url, resp => {
+        let data = "";
 
-    resp.on("data", chunk => {
-      data += chunk;
-    });
+        resp.on("data", chunk => {
+            data += chunk;
+        });
 
-    resp.on("end", () => {
-      try {
-        const apiResponse = JSON.parse(data);
-        if (resp.statusCode === 200) {
-          forecast = `${apiResponse.hours} hour forecast for ${
-            apiResponse.city
-          }, ${apiResponse.state}:\n${apiResponse.forecast}`;
-          message.channel.send(forecast);
-        } else {
-          handleError(resp.statusCode, apiResponse, message);
-        }
-      } catch (e) {
-        console.error(e.message);
-      }
+        resp.on("end", () => {
+            try {
+                const apiResponse = JSON.parse(data);
+                if (resp.statusCode === 200) {
+                    forecast = `${apiResponse.hours} hour forecast for ${
+                        apiResponse.city
+                        }, ${apiResponse.state}:\n${apiResponse.forecast}`;
+                    message.channel.send(forecast);
+                } else {
+                    handleError(resp.statusCode, apiResponse, message);
+                }
+            } catch (e) {
+                console.error(e.message);
+            }
+        });
     });
-  });
 }
 
 function handleError(statusCode, resp, message) {
-  let errorMessage;
-  switch (statusCode) {
-    case 404:
-      errorMessage = `${resp.info}
+    let errorMessage;
+    switch (statusCode) {
+        case 404:
+            errorMessage = `${resp.info}
             Your inputs:
             City: ${resp.city}
             State: ${resp.state}
             Period: ${resp.period}
             Please ensure you've spelled everything correctly, then try again.
             `
-        .replace(/^[\t ]+/gm, "")
-        .replace(/[\t ]+$/gm, "\n");
-      break;
-    case 500:
-      errorMessage = resp.message;
-      break;
-  }
-  message.channel.send(errorMessage);
+                .replace(/^[\t ]+/gm, "")
+                .replace(/[\t ]+$/gm, "\n");
+            break;
+        case 500:
+            errorMessage = resp.message;
+            break;
+    }
+    message.channel.send(errorMessage);
 }
 
 function helpMessage() {
-  return `
+    return `
     Usage:
 
     --------
@@ -161,6 +161,6 @@ function helpMessage() {
     Get a random forecast:
     (!thorcast|!thor|@Thorcast) random
     `
-    .replace(/^[\t ]+/gm, "")
-    .replace(/[\t ]+$/gm, "\n");
+        .replace(/^[\t ]+/gm, "")
+        .replace(/[\t ]+$/gm, "\n");
 }
